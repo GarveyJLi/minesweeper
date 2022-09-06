d56305385a88f6cc8fefb6c33cfe52c337f1caf3
GarveyJLi <GARVEY.J.LI@GMAIL.COM>
2022-09-05 19:37:50 -0700

    Fixed click on 0s and click on numcells with enough marked adjacent cells

--- button_types.py
+++ button_types.py
@@ -7,12 +7,14 @@ from numpy import random
 
 
 BUTTON_SIZE = 20
-ADJACENT_CELLS = [(0, 1), (1, 0), (1, 1), (-1, -1), (-1, 0), (0, -1), (1, -1), (-1, 1)]
+ADJACENT_CELLS = [(0, 1), (1, 0), (1, 1), (-1, -1), \
+    (-1, 0), (0, -1), (1, -1), (-1, 1)]
 
 
 class Cell:
     def __init__(self):
-        self.flag_image = Image.open('resources/flag.png').resize((BUTTON_SIZE, BUTTON_SIZE))
+        self.flag_image = Image.open('resources/flag.png').resize\
+            ((BUTTON_SIZE, BUTTON_SIZE))
         self.flag_image = ImageTk.PhotoImage(self.flag_image)
         self.marked = False
         self.hidden = True
@@ -31,8 +33,10 @@ class Cell:
     def get_adjacent(self, total_grid):
         for adjacent_cell in ADJACENT_CELLS:
             try:
-                single_cell = total_grid[self.xpos + adjacent_cell[0]][self.ypos + adjacent_cell[1]]
-                self.adjacent_cells.append(single_cell)
+                single_cell = total_grid[self.xpos + adjacent_cell[0]]\
+                    [self.ypos + adjacent_cell[1]]
+                if isinstance(single_cell, Cell):
+                    self.adjacent_cells.append(single_cell)
             except:
                 pass
         return self.adjacent_cells
@@ -41,24 +45,30 @@ class Cell:
         return self.marked
 
     def get_num_marked(self):
-        return len(list(filter(self.get_marked(), self.adjacent_cells))) #typerror bool object not callable
+        return len(list(filter(lambda x: x.get_marked(), self.adjacent_cells))) 
     
     def toggle_mark(self):
         if not self.marked:
                 self.marked = True
                 self.to_display = self.flag_image
-                self.button.config(image=self.to_display)
+                self.button.config(text=None, image=self.to_display)
         else:
             self.marked = False
             self.to_display = self.hidden_text
-            self.button.config(image=self.to_display)
+            self.button.config(text=self.to_display, image=None)
 
     def right_click(self, event):
         if self.hidden:
             self.toggle_mark()
+        else:
+            if self.num_bombs == self.get_num_marked():#gotta fix this bruh
+                    for cell in self.adjacent_cells:
+                        if not cell.get_marked():
+                            if cell.get_num_bombs == 0:
+                                cell.left_click()
+                            else:
+                                cell.reveal()
             
-
-    
 class NumCell(Cell):
     def __init__(self):
         super().__init__()
@@ -66,16 +76,19 @@ class NumCell(Cell):
         self.num_bombs = 0
 
     def reveal(self):
-        self.hidden = False
-        self.to_display = self.num_bombs
-        self.button.config(text=self.to_display)
+        if self.hidden:
+            self.hidden = False
+            self.to_display = self.num_bombs
+            self.button.config(text=self.to_display)
     
     def get_num_bombs(self):
-        self.num_bombs = len(list(filter(lambda x: isinstance(x, BombCell), self.adjacent_cells)))
+        self.num_bombs = len(list(filter(lambda x: \
+            isinstance(x, BombCell), self.adjacent_cells)))
         return self.num_bombs
 
     def create_button(self, frame, xpos, ypos):
-        self.button = Button(frame, text=self.to_display, command=self.left_click)
+        self.button = Button(frame, text=self.to_display, \
+            command=self.left_click)
         self.button.bind("<Button-3>", self.right_click)
         self.button.grid(row=xpos, column=ypos, sticky="ew")
         self.xpos = xpos
@@ -87,15 +100,19 @@ class NumCell(Cell):
                 if self.num_bombs == 0:
                     self.reveal()
                     for cell in self.adjacent_cells:
-                            if not cell.get_marked():
-                                cell.left_click()
+                        if not cell.get_marked():
+                            cell.left_click()
                 else:
                     self.reveal()
-            else:
-                if self.num_bombs == self.get_num_marked():       #gotta fix this bruh
+            """else:
+                if self.num_bombs == self.get_num_marked():#gotta fix this bruh
                     for cell in self.adjacent_cells:
                         if not cell.get_marked():
-                            cell.reveal()
+                            if cell.get_num_bombs == 0:
+                                cell.left_click()
+                            else:
+                                cell.reveal()"""
+            
 
         
         
@@ -103,7 +120,8 @@ class NumCell(Cell):
 class BombCell(Cell):
     def __init__(self):
         super().__init__()
-        self.bomb_image = Image.open('resources/bomb.png').resize((BUTTON_SIZE, BUTTON_SIZE))
+        self.bomb_image = Image.open('resources/bomb.png').resize\
+            ((BUTTON_SIZE, BUTTON_SIZE))
         self.bomb_image = ImageTk.PhotoImage(self.bomb_image)
 
     def reveal(self):
@@ -112,7 +130,8 @@ class BombCell(Cell):
         self.button.config(text=None, image=self.to_display)
     
     def create_button(self, frame, xpos, ypos):
-        self.button = Button(frame, text=self.to_display, command=self.left_click)
+        self.button = Button(frame, text=self.to_display, \
+            command=self.left_click)
         self.button.bind("<Button-3>", self.right_click)
         self.button.grid(row=xpos, column=ypos, sticky="ew")
         self.xpos = xpos
