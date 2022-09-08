de246d7b3c93a799616b0f956fded6f8ee0370d0
GarveyJLi <GARVEY.J.LI@GMAIL.COM>
2022-09-08 13:57:18 -0700

    About to change generate function and instance variables of Cells

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ total_grid = []
 def random_num(upper):
     return random.randint(upper)
 
-def generate(frame):
+def generate(frame): 
+    #rewrite so image generation is in generate function. maybe change get_adjacent to occur only when clicked.
+    #make total_grid an instance variable/attribute
     total_grid = [[None for c in range(COLUMNS)] for r in range(ROWS)]
 
     BOMB_COORDS.clear()
@@ -55,12 +57,16 @@ def main():
     root=Tk()
     root.resizable(height=None, width=None)
 
+    top_frame = Frame(root)
+    top_frame.grid(row=0, column=0)
+
     cell_frame = Frame(root)
     cell_frame.grid(row=1, column=0)
 
     smiley_image = Image.open('resources/smiley.jpg').resize((BUTTON_SIZE, BUTTON_SIZE))
     smiley_image = ImageTk.PhotoImage(smiley_image)
-    #new_game_button = Button(root, image=smiley_image, height=button_size, width=button_size).grid(row=0, column=0)
+    new_game_button = Button(top_frame, image=smiley_image, height=BUTTON_SIZE, width=BUTTON_SIZE, command=lambda: generate(cell_frame))
+    new_game_button.grid(row=0, column=0)
 
     generate(cell_frame)
 
