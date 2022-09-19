e1fac82613567cefb50c4a9d70ee1831c9bc2715
GarveyJLi <GARVEY.J.LI@GMAIL.COM>
2022-09-19 12:01:37 -0700

    Implemented difficulty select to change the number of bombs

--- main.py
+++ main.py
@@ -10,27 +10,25 @@ DIFFICULTIES = {
     'Medium': 0.2,
     'Hard': 0.3
 }
-ADJACENT_CELLS = [(0, 1), (1, 0), (1, 1), (-1, -1), (-1, 0), (0, -1), (1, -1), (-1, 1)]
+ADJACENT_CELLS = [(0, 1), (1, 0), (1, 1), (-1, -1), (-1, 0), (0, -1), \
+    (1, -1), (-1, 1)]
 BUTTON_SIZE = 20
 IMAGE_SIZE = 18
-
-total_grid = []
 
 def random_num(upper):
     return random.randint(upper)
 
 def generate(frame, top_frame, flag_image, bad_mark, bomb_image, red_bomb, \
-    rows, columns, bomb_coords, num_bombs, num_nums): 
-    #rewrite so image generation is in generate function. maybe change get_adjacent to occur only when clicked.
-    #make total_grid an instance variable/attribute
-    total_grid = [[None for c in range(columns)] for r in range(rows)]
-    button_types.reset()
+    rows, columns, bomb_coords, num_bombs, num_nums, total_grid, \
+        difficulty_var): 
 
+    total_grid = [[None for c in range(columns)] for r in range(rows)]
+    num_bombs = num_nums * DIFFICULTIES[difficulty_var.get()]
+    button_types.reset()
     bomb_coords.clear()
     while len(bomb_coords) < num_bombs:
         coord = (random_num(rows), random_num(columns))
         bomb_coords.add(coord)
-
     for r in range(rows):
         for c in range(columns):
             if (r, c) in bomb_coords:
@@ -43,21 +41,16 @@ def generate(frame, top_frame, flag_image, bad_mark, bomb_image, red_bomb, \
                     flag_image, bad_mark)    
                 new_num_cell.create_button(frame, r, c, bomb_coords)
                 total_grid[r][c] = new_num_cell
-
     for r in range(rows):
         for c in range(columns):
             current_cell = total_grid[r][c]
             current_cell.get_adjacent()
             if isinstance(current_cell, button_types.NumCell):
                 current_cell.get_num_bombs()
-    
     button_types.Cell.create_bomb_counter(top_frame)
 
-def difficulty_select(frame):
-    var = StringVar()
-    var.set("Easy")
-
-    options = OptionMenu(frame, var, "Easy", "Medium", "Hard")
+def difficulty_select(frame, difficulty_var):
+    options = OptionMenu(frame, difficulty_var, "Easy", "Medium", "Hard")
     options.grid(row=0, column=0)
 
 
@@ -67,17 +60,12 @@ def main():
 
     columns = 30
     rows = 20
-
     difficulty_var = StringVar()
     difficulty_var.set("Easy")
-
     num_nums = columns * rows
-    num_bombs = num_nums * DIFFICULTIES[difficulty_var.get()]
-
-    num_nums = columns * rows
-    num_bombs = num_nums * DIFFICULTIES[difficulty_var.get()]
+    num_bombs = 0
     bomb_coords = set()
-
+    total_grid = []
     flag_image = Image.open('resources/flag.png').resize\
         ((IMAGE_SIZE, IMAGE_SIZE))
     flag_image = ImageTk.PhotoImage(flag_image)
@@ -92,18 +80,14 @@ def main():
     red_bomb = ImageTk.PhotoImage(red_bomb)
     top_frame = Frame(root)
     top_frame.grid(row=0, column=0)
-
     cell_frame = Frame(root)
     cell_frame.grid(row=1, column=0)
-
     bottom_frame = Frame(root)
     bottom_frame.grid(row=2, column=0)
-
-    difficulty_select(bottom_frame)
-
+    difficulty_select(bottom_frame, difficulty_var)
     generate(cell_frame, top_frame, flag_image, bad_mark, bomb_image, \
-        red_bomb, rows, columns, bomb_coords, num_bombs, num_nums)
-    
+        red_bomb, rows, columns, bomb_coords, num_bombs, num_nums, \
+            total_grid, difficulty_var)
     smiley_image = Image.open('resources/smiley.jpg').resize((BUTTON_SIZE, \
         BUTTON_SIZE))
     smiley_image = ImageTk.PhotoImage(smiley_image)
@@ -111,11 +95,8 @@ def main():
         height=BUTTON_SIZE, width=BUTTON_SIZE, command=lambda: \
             generate(cell_frame, top_frame, flag_image, bad_mark, \
                 bomb_image, red_bomb, rows, columns, bomb_coords, \
-                    num_bombs, num_nums))
+                    num_bombs, num_nums, total_grid, difficulty_var))
     new_game_button.grid(row=0, column=1)
-
-
-
     root.mainloop()
-
+    
 main()
